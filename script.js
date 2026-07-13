e9ede5ee1a1f3cdd5e386acaae94ae63e64a1aec
Abhishek <webtest3035@gmail.com>
2026-07-13 17:00:43 +0530

    added pagination and filter buttons

--- script.js
+++ script.js
@@ -9,15 +9,47 @@ const taskUpdateSubmit = document.getElementById("taskUpdateSubmit");
 const table = document.getElementById("table");
 const taskSearchSubmit = document.getElementById("taskSearchSubmit");
 const taskFiltersButtons = document.getElementById("taskFiltersButtons");
+const pagination = document.getElementById("pagination");
+
+let allTasks = 0;
+let currentPage = 1;
+let itemsPerPage = 10;
+
+let filters = {
+    name: "",
+};
+
+let categories = {
+    find: ""
+};
+
+let status = {
+    find: ""
+};
 
 
 showTaskManager();
 
 async function showTaskManager() {
 
+    let url = `${tasksUrl}?_page=${currentPage}&_per_page=${itemsPerPage}`;
+
+    let total = `${tasksUrl}?`;
+
+    if (filters.name) {
+        url += `&taskName=${filters.name}`;
+    }
+
+    if (categories.find) {
+        url += `&_sort=${categories.find}`;
+    }
+    if (status.find) {
+        url += `&${status.find}`;
+    }
+
     try {
 
-        const response = await fetch(tasksUrl);
+        const response = await fetch(url);
 
         if (!response.ok) {
             throw new Error("Responce Is Not Ok");
@@ -25,7 +57,28 @@ async function showTaskManager() {
 
         let result = await response.json();
 
-        generateTable(result);
+        let tasks = result.data;
+
+        allTasks = result.items;
+
+        if (tasks.length === 0) {
+
+            alert("Not Found !");
+            return;
+        }
+
+        generateTable(tasks);
+
+        getDate();
+
+        createPagination();
+
+        const totalResponse = await fetch(total);
+
+        const totalData = await totalResponse.json();
+
+        document.getElementById("totalTask").textContent = totalData.length;
+        getTaskStatus(totalData);
 
     }
 
@@ -36,10 +89,12 @@ async function showTaskManager() {
 
 function generateTable(data) {
 
+    let allData = (currentPage - 1) * itemsPerPage;
+
     let rows = data.map((input, index) =>
-        `
-            <tr>
-                <td>${index + 1}</td>
+
+        ` <tr>
+                <td>${allData + index + 1}</td>
                 <td>${input.taskName}</td>
                 <td>${input.taskDescription}</td>
                 <td>${input.taskPriority}</td>
@@ -105,7 +160,7 @@ taskInputSubmit.addEventListener("click", async () => {
     }
 
     catch (error) {
-        console.error("Could Not Fetch Data:", error);
+        console.error("Could Not Add Data:", error);
     }
 
 })
@@ -178,7 +233,6 @@ async function editTask(id) {
 
 taskUpdateSubmit.addEventListener("click", async () => {
 
-
     let taskName = taskNameInput.value;
     let taskDescription = taskDescriptionInput.value;
     let taskPriority = taskPriorityInput.value;
@@ -224,6 +278,27 @@ taskUpdateSubmit.addEventListener("click", async () => {
     catch (error) {
         console.error("Could Not Update Data:", error);
     }
+
+})
+
+taskSearchSubmit.addEventListener("click", () => {
+
+    let input = document.getElementById("taskSearch").value;
+
+    document.getElementById("taskSearch").value = "";
+
+    if (input == "" || !isNaN(input)) {
+        alert("Invalid Search Input !");
+        return;
+    }
+
+    input = capitalInput(input);
+
+    filters.name = input;
+
+    currentPage = 1;
+
+    showTaskManager();
 
 })
 
@@ -282,3 +357,262 @@ function clearForm() {
     document.getElementById("taskStatus").selectedIndex = 0;
     editId = null;
 }
+
+taskFiltersButtons.addEventListener("click", (event) => {
+
+    if (event.target.id === "deadLineFirstToLast") {
+        deadLineFstToLst();
+    }
+
+    if (event.target.id === "deadLineLastToFirst") {
+        deadLineLstToFst();
+    }
+
+    if (event.target.id === "lowPriority") {
+        lowPriorityTas();
+    }
+
+    if (event.target.id === "mediumPriority") {
+        mediumPriorityTsk();
+    }
+
+    if (event.target.id === "highPriority") {
+        highPriorityTsk();
+    }
+
+    if (event.target.id === "urgentPriority") {
+        urgentPriorityTsk();
+    }
+
+    if (event.target.id === "pendingTask") {
+        pendingTsk();
+    }
+
+    if (event.target.id === "inProgressTask") {
+        inProgressTsk();
+    }
+
+    if (event.target.id === "CompletedTask") {
+        CompletedTsk();
+    }
+
+    if (event.target.id === "refresh") {
+        refresh();
+    }
+
+})
+
+function deadLineFstToLst() {
+
+    categories.find = "taskDeadline";
+
+    currentPage = 1;
+
+    showTaskManager();
+}
+
+function deadLineLstToFst() {
+
+    categories.find = "-taskDeadline";
+
+    currentPage = 1;
+
+    showTaskManager();
+
+}
+
+function lowPriorityTas() {
+
+    status.find = "taskPriority=Low";
+
+    currentPage = 1;
+
+    showTaskManager();
+}
+
+function mediumPriorityTsk() {
+
+    status.find = "taskPriority=Medium";
+
+    currentPage = 1;
+
+    showTaskManager();
+
+}
+
+function highPriorityTsk() {
+
+    status.find = "taskPriority=High";
+
+    currentPage = 1;
+
+    showTaskManager();
+
+
+}
+
+function urgentPriorityTsk() {
+
+    status.find = "taskPriority=Urgent";
+
+    currentPage = 1;
+
+    showTaskManager();
+}
+
+function pendingTsk() {
+
+    status.find = "taskStatus=Pending";
+
+    currentPage = 1;
+
+    showTaskManager();
+
+}
+
+function inProgressTsk() {
+
+    status.find = "taskStatus=In Progress";
+
+    currentPage = 1;
+
+    showTaskManager();
+
+}
+
+function CompletedTsk() {
+
+    status.find = "taskStatus=Completed";
+
+    currentPage = 1;
+
+    showTaskManager();
+
+}
+
+function refresh() {
+
+    document.getElementById("taskTable").innerHTML = "";
+
+    currentPage = 1;
+
+    filters.name = "";
+
+    categories.find = "";
+
+    status.find = "";
+
+    showTaskManager();
+
+}
+
+function getDate() {
+
+    const date = new Date();
+
+    const formatedDate = date.toLocaleDateString('en-GB', {
+        day: "numeric",
+        month: "short",
+        year: "numeric"
+    }).toLocaleUpperCase();
+
+    document.getElementById("heading-date").textContent = `Today: ${formatedDate}`;
+}
+
+function getTaskStatus(input) {
+
+    let pending = 0;
+
+    input.forEach(input => {
+        if (input.taskStatus === "Pending") {
+            pending++;
+        }
+    });
+
+    document.getElementById("totalPendingTask").textContent = pending;
+
+    let inPorgress = 0;
+
+    input.forEach(input => {
+        if (input.taskStatus === "In Progress") {
+            inPorgress++;
+        }
+    })
+
+    document.getElementById("TotalInProgressTask").textContent = inPorgress;
+
+    let Completed = 0;
+
+    input.forEach(input => {
+        if (input.taskStatus === "Completed") {
+            Completed++;
+        }
+    })
+
+    document.getElementById("TotalCompletedTask").textContent = Completed;
+
+}
+
+pagination.addEventListener("click", (event) => {
+
+    if (event.target.id === "previousPage") {
+        previous();
+    }
+    if (event.target.id === "nextPage") {
+        next();
+    }
+})
+
+function createPagination() {
+
+    let totalPages = Math.ceil(allTasks / itemsPerPage);
+
+    if (currentPage === 1) {
+
+        document.getElementById("previousPage").style.display = "none";
+    }
+    else {
+        document.getElementById("previousPage").style.display = "block";
+    }
+
+    if (currentPage === totalPages) {
+        document.getElementById("nextPage").style.display = "none";
+    }
+    else {
+        document.getElementById("nextPage").style.display = "block";
+    }
+
+}
+
+function pageChange() {
+
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+
+}
+
+function next() {
+
+    let totalPages = Math.ceil(allTasks / itemsPerPage);
+
+    if (currentPage < totalPages) {
+        currentPage++;
+        showTaskManager();
+        createPagination();
+        pageChange();
+    }
+}
+
+function previous() {
+
+    if (currentPage > 1) {
+        currentPage--;
+        showTaskManager();
+        createPagination();
+        pageChange();
+    }
+}
+
+
